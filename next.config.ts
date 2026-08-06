08842c27150a7facab693072989f249c25c05e8d
John Pat <your-github-email@example.com>
2026-08-05 20:08:03 -0400

    Update three song artworks

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    localPatterns: [
+      {
+        pathname: "/**",
+        search: "",
+      },
+      {
+        pathname: "/images/songs/**",
+        search: "?v=2",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
